b0d78dbab42ce5cafb98fd2a2017517f84eb71ff
Dylan McDonald <dmcdonald21@cnm.edu>
2016-10-02 20:19:30 -0600

    added webpack config

---
+++ webpack/index.php
@@ -0,0 +1,22 @@
+<?php
+// start session and enable XSRF protection
+require_once(dirname(__DIR__) . "/php/lib/xsrf.php");
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+setXsrfCookie();
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<base href="/" />
+
+		<title>My Experiences with Angular 2</title>
+	</head>
+	<body>
+		<abqwebgeeks-app>Loading&hellip;</abqwebgeeks-app>
+	</body>
+</html>
