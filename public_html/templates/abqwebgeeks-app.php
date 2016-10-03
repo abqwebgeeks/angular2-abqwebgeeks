2ab4733472d87c5717eda5e382a17a9f8360b1a9
Dylan McDonald <dmcdonald21@cnm.edu>
2016-10-02 21:08:45 -0600

    added sticky footer

--- public_html/templates/abqwebgeeks-app.php
+++ public_html/templates/abqwebgeeks-app.php
@@ -1,6 +1,12 @@
-<main class="container">
-	<router-outlet></router-outlet>
-</main>
-<footer>
-	<p>This is a footer</p>
-</footer>
+<div class="sfooter">
+	<div class="sfooter-content">
+		<main class="container">
+			<router-outlet></router-outlet>
+		</main>
+	</div>
+	<footer class="footer">
+		<div class="container">
+			<p>This is a footer</p>
+		</div>
+	</footer>
+</div>
