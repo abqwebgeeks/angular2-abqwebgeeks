93bcaac7f1d2cd0f79b66ccbb5051165aaae51b8
Dylan McDonald <dmcdonald21@cnm.edu>
2016-10-04 20:33:07 -0600

    added introduction

--- public_html/templates/introduction.php
+++ public_html/templates/introduction.php
@@ -1,1 +1,16 @@
 <h1>Introduction</h1>
+<hr />
+<h2>Who Am I?</h2>
+<p>I am <a href="https://www.deepdivedylan.com/">Deep Dive Dylan</a>. I teach the <a href="https://stemuluscenter.org/deep-dive-fullstack/">Deep Dive Full Stack</a> bootcamp at the <a href="https://stemuluscenter.org/">CNM STEMulus Center</a>. I have been in IT for 17 years and teaching in some capacity for 7 years. I have been a/an:</p>
+<ul>
+	<li>instructor</li>
+	<li>teaching assistant</li>
+	<li>enterprise software developer</li>
+	<li>PhD candidate</li>
+	<li>systems administrator</li>
+	<li>mentor</li>
+</ul>
+<p>Any questions about me? Just ask&hellip;</p>
+<hr />
+<h2>What is <img id="angularLogo" src="images/angular2.svg" alt="">ngular?</h2>
+<p>Angular is a self anoited "super heroic" framework that aims to be modular, generalized, and backend agnostic. It concentrates on using the component model to concentrate on reusing plain HTML and CSS in the form of templates to create dynamic views. More can be learned at the <a href="https://angular.io/">Angular web site</a>.</p>
