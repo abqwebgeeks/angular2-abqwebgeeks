46f0b773fafae3ccffaa7b9ee77af4a6c750ca0e
Dylan McDonald <dmcdonald21@cnm.edu>
2016-10-05 00:21:56 -0600

    added test template

--- public_html/templates/new-in-two.php
+++ public_html/templates/new-in-two.php
@@ -1,1 +1,10 @@
-<h1>New In 2</h1>
+<h1>What's New In 2?</h1>
+<hr />
+<h2>TypeScript</h2>
+<p>While TypeScript itself is not a feature of ...</p>
+<div class="row">
+	<div class="col-md-6" [innerHTML]="fooJs">
+	</div>
+	<div class="col-md-6" [innerHTML]="fooTs">
+	</div>
+</div>
