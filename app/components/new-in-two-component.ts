fbbb109f4a6f4f9f27039f0862d190bd92d2d92b
Dylan McDonald <dmcdonald21@cnm.edu>
2016-10-05 00:22:20 -0600

    modified component to load test code

--- app/components/new-in-two-component.ts
+++ app/components/new-in-two-component.ts
@@ -1,7 +1,28 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {HighlightService} from "../services/highlight-service";
 
 @Component({
 	templateUrl: "./templates/new-in-two.php"
 })
 
-export class NewInTwoComponent {}
+export class NewInTwoComponent implements OnInit {
+	fooJs : string = "";
+	fooTs : string = "";
+
+	constructor(private highlightService: HighlightService) {}
+
+	ngOnInit() : void {
+		this.getFooJs();
+		this.getFooTs();
+	}
+
+	getFooJs() : void {
+		this.highlightService.getHighlight("foo.js")
+			.subscribe(fooJs => this.fooJs = fooJs);
+	}
+
+	getFooTs() : void {
+		this.highlightService.getHighlight("foo.ts")
+			.subscribe(fooTs => this.fooTs = fooTs);
+	}
+}
